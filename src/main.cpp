61338cd402e6957ab69cba02dcbab4319274599b
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2023-07-06 09:55:13 -0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+#include "read.hpp"
+
+int main() {
+    std::clock_t start = std::clock();
+
+    ReadFile();
+
+    std::clock_t end = std::clock();
+    double duration = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+
+    std::cout << "\nTempo total de execução: " << duration << " segundos." << std::endl;
+
+    return 0;
+}
